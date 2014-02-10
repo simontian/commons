fdd48b480acd610d21bdfe5a4f43bccf99532997
simontian <tianwenb@gmail.com>
2014-02-10 15:48:54 +0800

    show tool version

--- src/main/java/org/commons/Version.java
+++ src/main/java/org/commons/Version.java
@@ -1,9 +1,37 @@
 package org.commons;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class Version {
 
 	public static void main(String[] args) {
-
+		
+		try {
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("pom.xml");
+			Element element = document.getDocumentElement();
+			NodeList nodes = element.getChildNodes();
+			String version = null;
+			for(int i = 0;i < nodes.getLength();i++) {
+				Node node = nodes.item(i);
+				if("version".equals(node.getNodeName())) {
+					version = node.getTextContent();
+					break;
+				}
+			}
+			if(version == null || version == "") {
+				System.out.println("Version is unknown");
+			} else {
+				System.out.println("Version is " + version);
+			}
+		} catch(Exception e) {
+			System.out.println("Version is unknown");
+		}
+		
 	}
 
 }
